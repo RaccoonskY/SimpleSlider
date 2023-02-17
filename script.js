c791523b3da4c3c5e779cac4fa08aa1e7028bf14
RaccoonskY <viktoryablonskas@yandex.ru>
2023-02-17 19:42:49 +0300

    refactored

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+
+
 var images = [
     'http://www.sololearn.com/uploads/slider/1.jpg',
     'http://www.sololearn.com/uploads/slider/2.jpg',
     'http://www.sololearn.com/uploads/slider/3.jpg'
 ];
 var num = 0;
-var slider = document.getElementById('slider');
-
-
 function next() {
+    var slider = document.getElementById('slider');
     num++;
     if (num >= images.length) {
         num = 0;
@@ -17,6 +17,7 @@ function next() {
 
 
 function prev() {
+    var slider = document.getElementById('slider');
     num--;
     if(num < 0) {
         num = images.length-1;
