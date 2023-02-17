4f6e676694068402308e27de0482e4dbf0e822e9
RaccoonskY <viktoryablonskas@yandex.ru>
2023-02-17 19:32:32 +0300

    refactored example1.js

--- example1.js
+++ example1.js
@@ -4,16 +4,19 @@ var images = [
     'http://www.sololearn.com/uploads/slider/3.jpg'
 ];
 var num = 0;
+var slider = document.getElementById('slider');
+
+
 function next() {
-    var slider = document.getElementById('slider');
     num++;
-    if(num >= images.length) {
+    if (num >= images.length) {
         num = 0;
     }
     slider.src = images[num];
 }
+
+
 function prev() {
-    var slider = document.getElementById('slider');
     num--;
     if(num < 0) {
         num = images.length-1;
